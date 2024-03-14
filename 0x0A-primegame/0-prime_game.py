dd570e2d4633ecb4b4a6fb2f9c3f9c485b52ca87
Akwesi-Bona <arhinbonnah@gmail.com>
2024-03-14 07:41:17 +0000

    task completed

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" The Prime Game"""
+
+
+def isWinner(x, nums):
+    """
+    Determines the winner of the prime game.
+
+    Args:
+        x (int): The number of rounds to be played.
+        nums (List[int]): A list of integers representing the number of elements in each round.
+
+    Returns:
+        str: The name of the winner ('Maria' or 'Ben') or None if there is no winner.
+
+    """
+    if x < 1 or not nums:
+        return None
+
+    marias_wins = 0
+    bens_wins = 0
+    max_num = max(nums)
+    primes = [True] * (max_num + 1)
+    primes[0] = primes[1] = False
+
+    for i in range(2, int(max_num ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, max_num + 1, i):
+                primes[j] = False
+
+    for n in nums:
+        primes_count = sum(primes[:n])
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+
+    if marias_wins == bens_wins:
+        return None
+
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
+
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,6 @@
+# Prime Game
+
+The prime game is a simple game where two players take turns selecting a number from a given range. The goal of the game is to select prime numbers, and the player with the highest sum of prime numbers at the end of the game wins.
+
+To implement the prime game in Python, you can follow these steps:
+
